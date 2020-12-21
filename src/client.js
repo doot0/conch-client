5cc6c65ba5e33e7c4334591bbb292f7815a55505
Dave <doot0@doot0.co.uk>
2020-12-21 01:29:47 +0000

    Add more flexible message printing, start namechange functionality

--- src/client.js
+++ src/client.js
@@ -1,11 +1,45 @@
 'use strict';
 
+const CONCH_PREFIX = '--conch';
 const SERVER_ENDPOINT = 'http://localhost:5001';
 const socket = io(SERVER_ENDPOINT);
 
 const getStyles = (type = 'msg') => {
-  const styleString = getComputedStyle(document.documentElement).getPropertyValue(`--conch-${type}`)
+  const styleString = getComputedStyle(document.documentElement).getPropertyValue(`${CONCH_PREFIX}-${type}`)
   return styleString.slice(1, -1);
+}
+
+const introChip = () => {
+  if (!!document.querySelector(`.${CONCH_PREFIX}-logo`)) return;
+  
+  const target = document.body;
+  const chip = document.createElement('span');
+  const icon = document.createElement('span');
+  
+  chip.classList.add(`${CONCH_PREFIX}-logo`);
+  
+  icon.innerText = '🐚';
+  chip.setAttribute('style', STYLES.INTRO_CHIP);
+  icon.setAttribute('style', STYLES.INTRO_ICON);
+  
+  chip.appendChild(icon);
+  target.appendChild(chip);
+  
+  setTimeout(() => {
+    chip.style.opacity = 0;
+  }, 2000);
+}
+
+const assignName = (name) => {
+  
+}
+
+const STYLES = {
+  INTRO_CHIP: getStyles('logo'),
+  INTRO_ICON: getStyles('logo-icon'),
+  MSG_NAME: getStyles('username'),
+  MSG_INFO: getStyles('info-msg'),
+  MSG_MSG: getStyles()
 }
 
 const ui = {
@@ -15,15 +49,15 @@ const ui = {
   disconnect: (reason) => {
     console.info('Disconnected from server. Reason ->', reason)
   },
-  message: (body) => {
-    const nameStyle = getStyles('username')
-    const msgStyle = getStyles()
-    console.log(`%c${socket.id} %c${body}`, nameStyle, msgStyle)
-  }
+  message: (body, prefix, infoStyle = `MSG_NAME`, messageStyle = `MSG_MSG`) => {
+    if (!prefix) prefix = socket.id;
+    console.log(`%c${prefix} %c${body}`, STYLES[infoStyle], STYLES[messageStyle])
+  },
 }
 
 socket.on("connect", () => {
-  ui.connect(SERVER_ENDPOINT)
+  introChip();
+  ui.connect(SERVER_ENDPOINT);
   console.log(`UID: ${socket.id}`);
 });
 
@@ -35,12 +69,24 @@ socket.on("message", (message) => {
   ui.message(message.body)
 })
 
+socket.on("namechange", (name) => {
+  ui.message(`${name.body}`, `NC:`, `MSG_INFO`);
+})
+
 const send = (message) => {
   socket.emit('message', {
     body: `${message}`
   })
 }
 
+const setName = (name) => {
+  assignName(name);
+  socket.emit('namechange', {
+    body: name
+  })
+}
+
 const Conch = {
-  say: send
+  say: send,
+  setName
 }
